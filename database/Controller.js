82eb2101307cf11c3283ad82d60bfa1366496606
cpascasio <christian_james_pascasio@dlsu.edu.ph>
2023-06-27 01:45:19 +0800

    newww

---
+++ database/Controller.js
@@ -0,0 +1,136 @@
+const db = require('./db');
+const Order = require('../template/order');
+
+
+// GET /api/orders
+/*
+gets all the orders
+*/
+exports.getOrders = (req, res, next) => {
+    db.query('SELECT * FROM orders', (err, results) => {
+            if (err) {
+            console.error('Error executing MySQL query:', err);
+            res.status(500).send('Error executing query');
+            }else{
+            res.json(results);
+            }
+        });
+}
+
+// GET /api/orders/:id
+
+exports.getOrder = (req, res, next) => {
+    const id = req.params.id;
+    db.query('SELECT * FROM orders WHERE orderID = ?', [id], (err, results) => {
+            if (err) {
+            console.error('Error executing MySQL query:', err);
+            res.status(500).send('Error executing query');
+            }else{
+            res.json(results);
+            }
+        });
+}
+
+
+// POST /api/orders
+
+exports.createOrder = (req, res, next) => {
+    const name = req.body.name;
+    const main = req.body.main;
+    const side = req.body.side;
+    const drink = req.body.drink;
+    const m1 = req.body.m1;
+    const s1 = req.body.s1;
+    const d1 = req.body.d1;
+    const CMT = req.body.CMT;
+    const SVB = req.body.SVB;
+    const final = req.body.final;
+
+    const order = new Order(name, main, side, drink, m1, s1, d1, CMT, SVB, final);
+
+    db.query('INSERT INTO orders SET ?' , [order], (err, results) => {
+            if (err) {
+            console.error('Error executing MySQL query:', err);
+            res.status(500).send('Error executing query');
+            }else{
+            res.json(results);
+            }
+        });
+}
+
+
+/*
+exports.createOrder = (req, res, next) => {
+    const main = "STEAK";
+    const side = "STEAMED VEGETABLES";
+    const drink = "WATER";
+    const m1 = 1;
+    const s1 = 2;
+    const d1 = 1;
+
+    
+    const main = req.body.main;
+    const side = req.body.side;
+    const drink = req.body.drink;
+    const m1 = req.body.m1;
+    const s1 = req.body.s1;
+    const d1 = req.body.d1;
+
+    
+    const order = new Order(main, side, drink, m1, s1, d1);
+
+    console.log(order);
+
+    db.query('INSERT INTO practice SET ?', [order], (err, results) => {
+            if (err) {
+            console.error('Error executing MySQL query:', err);
+            res.status(500).send('Error executing query');
+            }else{
+            res.json(results);
+            }
+        });
+}
+
+*/
+
+// PUT /api/orders
+
+exports.updateOrder = (req, res, next) => {
+    const id = req.params.id;
+    const name = req.body.name;
+    const main = req.body.main;
+    const side = req.body.side;
+    const drink = req.body.drink;
+    const m1 = req.body.m1;
+    const s1 = req.body.s1;
+    const d1 = req.body.d1;
+    const price = req.body.price;
+    const CMT = req.body.CMT;
+    const SVB = req.body.SVB;
+    const final = req.body.final;
+    const date = req.body.date;
+
+    const order = new Order(id, name, main, side, drink, m1, s1, d1, price, CMT, SVB, final, date);
+
+    db.query('UPDATE orders SET ? WHERE orderID = ?', [order, id], (err, results) => {
+            if (err) {
+            console.error('Error executing MySQL query:', err);
+            res.status(500).send('Error executing query');
+            }else{
+            res.json(results);
+            }
+        });
+}
+
+
+exports.deleteOrders = (req, res, next) => {
+    const id = req.params.id;
+    db.query('DELETE FROM orders WHERE orderID = ?', [id], (err, results) => {
+            if (err) {
+            console.error('Error executing MySQL query:', err);
+            res.status(500).send('Error executing query');
+            }else{
+            res.json(results);
+            }
+        });
+}
---
+++ template/order.js
@@ -0,0 +1,34 @@
+const moment = require('moment');
+let now = moment();
+
+class order{
+    constructor(name, main, side, drink, m1, s1, d1, price, CMT, SVB, final){
+    this.name = name;
+    this.main = main;
+    this.side = side;
+    this.drink = drink;
+    this.m1 = m1;
+    this.s1 = s1;
+    this.d1 = d1;
+    this.price = price;
+    this.CMT = CMT;
+    this.SVB = SVB;
+    this.final = final;
+    this.date = now.format("YYYY-MM-DD") + " " + now.format("HH:mm:ss");
+    }
+    
+
+    /*
+    constructor (main, side, drink, m1, s1, d1){
+    this.main = main;
+    this.side = side;
+    this.drink = drink;
+    this.m1 = m1;
+    this.s1 = s1;
+    this.d1 = d1;
+        }
+
+        */
+}
+
+module.exports = order;
